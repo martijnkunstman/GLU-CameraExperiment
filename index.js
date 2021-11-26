03ccf68d27f7979d856cb6d160f06780bd8347f0
martijnkunstman <martijn@kunstman.nl>
2021-11-26 14:27:11 +0100

    remove filter

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const base64ToImage = require('base64-to-image');
-const uniqid = require('uniqid');
+const base64ToImage = require('base64-to-image')
+const uniqid = require('uniqid')
 
 //set up express server
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
 
 //serve files in public folder (publix/index.html)
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static('public'))
+app.use(express.json())
 
 //use the file system
-const fs = require('fs');
+const fs = require('fs')
 
 app.post('/postdata', (req, res) => {
-    const base64Str = req.body.postData;
-    let optionalObj = { 'fileName': uniqid(), 'type': 'png' };
-    base64ToImage(base64Str, "./public/img/", optionalObj);
+    const base64Str = req.body.postData
+    let optionalObj = { 'fileName': uniqid(), 'type': 'png' }
+    base64ToImage(base64Str, "./public/img/", optionalObj)
     res.send({ "status": "ok" })
 })
 
 //load content of data.txt
 app.get('/data', (req, res) => {
-    let data = [];
-    fs.readdirSync("./public/img/").forEach(file => {data.push(file);});
+    let data = []
+    fs.readdirSync("./public/img/").forEach(file => { data.push(file) })
     res.send({ "data": data })
 })
--- public/script.js
+++ public/script.js
@@ -1,40 +1,23 @@
 const constraints = { video: true, };
 const captureVideoButton = document.querySelector("#cssfilters .capture-button");
-const cssFiltersButton = document.querySelector("#cssfilters-apply");
 const saveImage = document.querySelector("#saveImage");
 const loadImage = document.querySelector("#loadImage");
 const video = document.querySelector("#cssfilters video");
 const dataurl_container = document.querySelector("#dataurl-container");
 const loadedImage = document.querySelector("#loadedImage");
-const filters = ["grayscale", "sepia", "blur", "brightness", "contrast", "hue-rotate", "hue-rotate2", "hue-rotate3", "saturate", "invert", "",];
-
-let filterIndex = 0;
 
 captureVideoButton.onclick = function () {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(handleSuccess)
-        .catch(handleError);
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 };
 
-cssFiltersButton.onclick = video.onclick = function () {
-    video.className = filters[filterIndex++ % filters.length];
-};
-
-function handleSuccess(stream) {
-    video.srcObject = stream;
-}
-
-function handleError(error) {
-    console.error("Error: ", error);
-}
+function handleSuccess(stream) { video.srcObject = stream; }
+function handleError(error) { console.error("Error: ", error); }
 
 saveImage.onclick = function () {
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
     let image_data_url = canvas.toDataURL('image/jpeg');
     dataurl.value = image_data_url;
     dataurl_container.style.display = 'block';
-    //
     fetch('/postdata', {
         method: 'post',
         headers: {
@@ -45,7 +28,6 @@ saveImage.onclick = function () {
     }).then(res => res.json()).then(res => { console.log(res.status); });
 }
 
-
 loadImage.onclick = function () {
     fetch('/data')
         .then(res => res.json())
@@ -53,10 +35,10 @@ loadImage.onclick = function () {
             console.log(res.data);
             //empty div
             document.getElementById('loadedImage').innerHTML = "";
-            //create all images
+            //show all images in the array
             for (a = 0; a < res.data.length; a++) {
                 var img = document.createElement('img');
-                img.src = "./img/"+res.data[a];
+                img.src = "./img/" + res.data[a];
                 document.getElementById('loadedImage').appendChild(img);
             }
         });
