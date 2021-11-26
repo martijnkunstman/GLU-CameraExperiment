043a5e69df45a22e6d6e9063854adefb11cbf138
martijnkunstman <martijn@kunstman.nl>
2021-11-26 18:13:20 +0100

    cleanup

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
 
-//serve files in public folder (publix/index.html)
+//serve files in public folder (public/index.html)
 app.use(express.static('public'))
 app.use(express.json())
 
